374dc315dcb59117187e4331d3cd60ede887ac4a
thomasya <thomas_ya@outlook.com>
2020-04-01 22:09:33 +0800

    Edit import bug

--- bot/views.py
+++ bot/views.py
@@ -10,7 +10,7 @@ from linebot.exceptions import (
     InvalidSignatureError, LineBotApiError
 )
 from linebot.models import *
-from module import Debt_Simplification
+from module/Debt_Simplification import *
 
 
 
